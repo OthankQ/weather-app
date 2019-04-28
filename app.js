b0e7ae4a1e40cfdef16bbfa4811c98e8771da1c7
Ryan <kim1885@purdue.edu>
2019-04-28 00:54:45 -0400

    change input method using process and argv

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-readline.question('Enter your location: ', location => {
-  geocode(location, (error, data) => {
+const location = process.argv[2];
+
+geocode(location, (error, data) => {
+  if (error) {
+    return console.log(error);
+  }
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
     if (error) {
       return console.log(error);
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log(data.location);
-      console.log(forecastData);
-    });
+    console.log(data.location);
+    console.log(forecastData);
   });
 });
